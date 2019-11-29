7f9c1973501ba98ec45fbb05967611b91ff3d3fc
Mikhail Orlov <mikhail.orlov@intive.com>
2019-11-29 17:16:18 +0100

    m

--- build_redis.sh
+++ build_redis.sh
@@ -1,6 +1,12 @@
+# Exits on errors and logs commands with "+" before executing them.
 set -ex
+
+# Script full path and its directory full path.
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIRPATH=$(dirname "$SCRIPT")
+
 mkdir BUILD &2>/dev/null
 
 rpmbuild -ba SPECS/redis.spec \
   --undefine=_disable_source_fetch \
-  --buildroot BUILD
+  --buildroot $SCRIPTDIRPATH/BUILD
