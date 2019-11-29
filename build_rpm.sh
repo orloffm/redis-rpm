24bfc1263dd9e036974e0a6cd74dd8e9a54857e7
Mikhail Orlov <orloffm@gmail.com>
2019-11-29 15:20:14 +0000

    location

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,10 +1,13 @@
+
 set -ex
 
-whereiam=${.sh.file%/*}
-root_tree=${whereiam%/*}
+
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 
 #rpmbuild -ba SPECS/prcredis.spec \
 #  --undefine=_disable_source_fetch
 
-echo $whereiam
-echo $root_tree
+echo $SCRIPTPATH
