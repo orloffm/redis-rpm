bc819a730ea452082c0dcfbf69ca691b288fd914
Mikhail Orlov <mikhail.orlov@intive.com>
2019-11-29 16:03:35 +0100

    build

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,1 +1,10 @@
-rpmbuild -ba SPECS/prcredis.spec
+set -ex
+
+whereiam=${.sh.file%/*}
+root_tree=${whereiam%/*}
+
+#rpmbuild -ba SPECS/prcredis.spec \
+#  --undefine=_disable_source_fetch
+
+echo $whereiam
+echo $root_tree
